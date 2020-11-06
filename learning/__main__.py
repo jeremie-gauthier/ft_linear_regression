831c5f6cef447d2cedbac7afeb5a3f033e8191a0
Jeremie <jeremiegthr@gmail.com>
2020-11-06 10:35:25 +0100

    add learning __main__

--- learning/__main__.py
+++ learning/__main__.py
@@ -1,7 +1,22 @@
+from utils import csv
+from utils.load_thetas import load_thetas
 from .src.Data import Data
-from .src.cost_function import cost_fn
+from .src.gradient_descent import gradient_descent
+
+
+def load_dataset():
+    df = csv.load("./learning/data.csv")
+
+    kms = csv.dataframe(df, "km")
+    prices = csv.dataframe(df, "price")
+    plots = [Data(x, y) for x, y in zip(kms, prices)]
+    return plots
+
 
 if __name__ == "__main__":
-    plots = [Data(1, 1), Data(2, 2), Data(3, 3)]
-    print(cost_fn(0, 0, plots))
+    plots = load_dataset()
+    initial_thetas = load_thetas()
 
+    final_thetas = gradient_descent(initial_thetas[0], initial_thetas[1], plots)
+
+    csv.write_thetas("./thetas.csv", final_thetas)
