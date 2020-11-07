e1e5eecd23e469853a9a3b20cd9e8e9e91bdf342
Jeremie <jeremiegthr@gmail.com>
2020-11-07 12:23:31 +0100

    normalize user input

--- prediction/__main__.py
+++ prediction/__main__.py
@@ -1,13 +1,24 @@
+import argparse
 from utils import csv
-from utils.load_thetas import load_thetas
+from utils.load_data import load_thetas, load_dataset
 from .src.estimate_price import estimate_price
+from utils.normalization import normalize, denormalize
+
+
+def prediction(args):
+    thetas = load_thetas()
+    kms, prices = load_dataset()
+    norm_mileage = normalize(args.mileage, kms)
+    estimation = denormalize(estimate_price(*thetas, norm_mileage), prices)
+
+    print(f"Estimation for {args.mileage} is {round(estimation, 2)}")
+
 
 if __name__ == "__main__":
-    theta_0, theta_1 = load_thetas()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "mileage", help="set the mileage to estimate", type=float,
+    )
 
-    # catch input error
-    mileage = float(input("Kilometrage: "))
-
-    estimation = estimate_price(theta_0, theta_1, mileage)
-
-    print(f"Estimation for {mileage} with t0={theta_0} and t1={theta_1} = {estimation}")
+    args = parser.parse_args()
+    prediction(args)
