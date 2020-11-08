cc78c08a6314fe833262475606b67ff326a6a077
Jeremie <jeremiegthr@gmail.com>
2020-11-08 18:00:54 +0100

    use the cost_fn to detect convergence or divergence

--- learning/src/gradient_descent.py
+++ learning/src/gradient_descent.py
@@ -1,5 +1,6 @@
 from .cost_function import cost_fn
 from .derivative_thetas import derivative_thetas
+import sys
 
 
 def _simultaneous_update(theta_0, theta_1, plots, learning_rate):
@@ -9,10 +10,36 @@ def _simultaneous_update(theta_0, theta_1, plots, learning_rate):
     return (new_theta_0, new_theta_1)
 
 
+def _precision(plots):
+    cost = 1
+    prev_cost = 0
+
+    def convergence(theta_0, theta_1):
+        nonlocal cost, prev_cost
+
+        cost = cost_fn(theta_0, theta_1, plots)
+        if abs(prev_cost - cost) < 0.00000000000001:
+            return True
+        if abs(cost) > 1000000:
+            print(
+                "[-] There is a divergence in the results. Decrease the learning rate.",
+                file=sys.stderr,
+            )
+            sys.exit(-1)
+        prev_cost = cost
+        return False
+
+    return convergence
+
+
 def gradient_descent(theta_0, theta_1, plots, learning_rate, iterations):
     new_theta_0 = 0
     new_theta_1 = 0
+    convergence = _precision(plots)
     for i in range(iterations):
+        if convergence(new_theta_0, new_theta_1):
+            return (new_theta_0, new_theta_1)
+
         new_theta_0, new_theta_1 = _simultaneous_update(
             new_theta_0, new_theta_1, plots, learning_rate
         )
